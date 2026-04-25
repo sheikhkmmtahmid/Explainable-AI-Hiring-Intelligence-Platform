126c6dc403be4bfbdabe502ce0bf3eec23726818
sheikhkmmtahmid <sheikh.k.m.m.tahmid@gmail.com>
2026-04-25 13:18:48 +0100

    Switch database engine to MySQL for TiDB Cloud

--- config/settings/base.py
+++ config/settings/base.py
@@ -77,12 +77,17 @@ ASGI_APPLICATION = "config.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME", default="hiringai_db"),
-        "USER": config("DB_USER", default="hiringai"),
-        "PASSWORD": config("DB_PASSWORD", default="hiringai_pass"),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": config("DB_NAME", default="hiringai"),
+        "USER": config("DB_USER", default="root"),
+        "PASSWORD": config("DB_PASSWORD", default=""),
         "HOST": config("DB_HOST", default="localhost"),
-        "PORT": config("DB_PORT", default="5432"),
+        "PORT": config("DB_PORT", default="4000"),
+        "OPTIONS": {
+            "ssl": {"ca": None},
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
         "CONN_MAX_AGE": 60,
     }
 }
